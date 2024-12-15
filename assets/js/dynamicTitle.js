36adae4074b6a5f8eccfcfb9219930529ffe9e6c
Sarathi7777 <sarathiff777@gmail.com>
2024-12-15 17:11:11 +0530

    initial

--- assets/js/dynamicTitle.js
+++ assets/js/dynamicTitle.js
@@ -11,7 +11,7 @@ window.onload = function () {
   function toggle(isPageActive) {
     if (isPageActive) {
       document.title = pageTitle
-      favicon.href = './assets/images/sarathi.png'
+      favicon.href = './assets/images/dp_male'
     } else {
       document.title = attentionMessage
       favicon.href = './assets/images/folded.png'
