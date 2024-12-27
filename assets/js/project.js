c10ae4f6bfc298f95261d78af083461b73b769ef
sarathi_20 <142975260+Sarathi7777@users.noreply.github.com>
2024-12-27 15:34:11 +0530

    Update project.js

--- assets/js/project.js
+++ assets/js/project.js
@@ -10,7 +10,7 @@ const projects = [
     cardImage: "assets/images/project-page/geofence.jpg",
     description: "A quiz app built using basic react.",
     tagimg: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
-    Previewlink: "",
+    Previewlink: "https://glbat.vercel.app/",
     Githublink: "",
   },
   {
